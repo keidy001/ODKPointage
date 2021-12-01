0c8ab4019128209b446b9fdbcad5554058db7adb
Fadiala Sidibé <fadiala.sidibe@odc.local>
2021-12-01 14:37:49 +0000

    Fadiala

--- src/app/Admin/add-admin/add-admin.component.ts
+++ src/app/Admin/add-admin/add-admin.component.ts
@@ -17,34 +17,28 @@ export class AddAdminComponent implements OnInit {
   id: any;
   chaine : string;
   loginData: any;
+  userId:any;
+  userngForm: NgForm;
   
   submitted = false;
 
   constructor(
     public service: UsersService,
     public  route: ActivatedRoute,
-<<<<<<< HEAD
     public router : Router,
     public formBuilder: FormBuilder) { }
-=======
-    public router : Router) { }
->>>>>>> 8195d36eb78491e26b54a3c564f860e9de8cf8cd
 
   ngOnInit(): void {
-    // this.form = this.formBuilder.group({
-    //   email: [null, [Validators.required, Validators.email]],
-    //   nom:[null, [Validators.required]],
-    //   prenom:[null, [Validators.required]],
 
-<<<<<<< HEAD
-    // })
-=======
 
->>>>>>> 8195d36eb78491e26b54a3c564f860e9de8cf8cd
-    console.log(this.adminData);
+    // this.userId =  this.userngForm.value.userId;
+    // console.log(this.userId);
+
+
     this.loginData=JSON.parse(localStorage["isLogin"]);
 
     this.formgroup = this.formBuilder.group({
+
       nom: ['', Validators.required],
       prenom: ['', Validators.required],
       genre: ['', Validators.required],
@@ -55,76 +49,83 @@ export class AddAdminComponent implements OnInit {
       etat: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       motDePass: ['', [Validators.required, Validators.minLength(6)]],
+      
+
       //confirmPassword: ['', Validators.required],
       //acceptTerms: [false, Validators.requiredTrue] //Checkbox For accept conditions 
   },);
   }
 
-<<<<<<< HEAD
+
+  // ajouterAdmin(addForm: NgForm){
+  //   //this.chaine = addForm.value.profile;
+  //   if (addForm.valid){
+  //     var obj: { [id: string]: any} = {};
+
+     
+  //      obj.id = addForm.value.profile; 
+  //      addForm.value.profile = obj;
+  
+  //     console.log(JSON.stringify(addForm.value));
+  
+  //     this.service.addAdmin(addForm.value).subscribe(
+        
+  //       (data)=>{
+  //         this.router.navigateByUrl("listAdmins");
+  //         this.userId =  this.userngForm.value.userId;
+  //         console.log(this.userId);
+        
+  //         console.log("hello world" +data);         
+  //       }
+  //     )
+  //   }else{
+  //     console.log("Not valid...")
+  //   }
+  // }
+
+
+
+
+
+
+
+
 
   get f() { return this.formgroup.controls; }
-  ajouterAdmin(addForm: NgForm){
-    //this.chaine = addForm.value.profile;
-    if (addForm.valid){
-      var obj: { [id: string]: any} = {};
-     
-       obj.id = addForm.value.profile; 
-       addForm.value.profile = obj;
-  
-      console.log(JSON.stringify(addForm.value));
-  
-      this.service.addAdmin(addForm.value).subscribe(
-        
-        (data)=>{
-          this.router.navigateByUrl("listAdmins");
-        
-          console.log("hello world" +data);         
-        }
-      )
-    }else{
-      console.log("Not valid...")
-    }
-  }
 
-  hello(){
+
+
+
+  hello(fg : FormGroup){
     this.submitted = true;
+    
+
 
     // stop here if form is invalid
     if (this.formgroup.invalid) {
         return;
     }
 
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.formgroup.value, null, 4));
-=======
-  ajouterAdmin(addForm: NgForm){
-    //this.chaine = addForm.value.profile;
     var obj: { [id: string]: any} = {};
+     
+    obj.id = fg.value.profile; 
+    fg.value.profile = obj;
+    fg.value.userId =this.loginData.id
+     
+   console.log(JSON.stringify(fg.value));
 
-     obj.id = addForm.value.profile; 
-     addForm.value.profile = obj;
-
-    console.log(JSON.stringify(addForm.value));
-
-    this.service.addAdmin(addForm.value).subscribe(
-
-      (data)=>{
-        this.router.navigateByUrl("listAdmins");
-
-        console.log("hello world" +data);
-
-
-
-      }
-    )
->>>>>>> 8195d36eb78491e26b54a3c564f860e9de8cf8cd
+   this.service.addAdmin(fg.value).subscribe(
+     
+     (data)=>{
+       this.router.navigateByUrl("/listAdmins");
+     
+       console.log("hello world" +data);         
+     }
+   )
+    
   }
   logOut(){
     localStorage.removeItem('isLogin');
   this.router.navigateByUrl('/');
-<<<<<<< HEAD
-}
-=======
->>>>>>> 8195d36eb78491e26b54a3c564f860e9de8cf8cd
 }
 }
